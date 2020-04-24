30cce06784ab48ead460644e94cc78346437a110
quackers71 <24840026+Quackers71@users.noreply.github.com>
2020-04-24 19:17:10 +0100

    using a constant

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ console.log(name);
 let firstName = "Rob";
 let surName = "Q";
 let fullName = firstName.concat(" "+surName);
+
 console.log(fullName);
+
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
