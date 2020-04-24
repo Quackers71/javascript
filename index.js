044aec3828c55e1414b4f3ee1b89a85d399f1aa8
quackers71 <24840026+Quackers71@users.noreply.github.com>
2020-04-24 17:40:53 +0100

    ran node from VSC Terminal

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-// This is my first JS Code for a while now!
-console.log("Hello World");
+
+let name = 'Yo Bro';
+console.log(name);
+
+let firstName = "Rob";
+let surName = "Q";
+let fullName = firstName.concat(" "+surName);
+console.log(fullName);
