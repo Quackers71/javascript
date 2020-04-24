1bfa67700d0f2c6265910ed507a427e9c2794b2d
quackers71 <24840026+Quackers71@users.noreply.github.com>
2020-04-24 20:35:29 +0100

    Primitives/Value Types

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 let name = 'Yo Bro';
 console.log(name);
 
-let firstName = "Rob";
-let surName = "Q";
+let firstName = "Rob"; // Dynamically typed - let
+let surName = "Q"; // Statically typed - string
 let fullName = firstName.concat(" "+surName);
 
 console.log(fullName);
@@ -11,3 +11,9 @@ console.log(fullName);
 const interestRate = 0.3;
 interestRate = 1;
 console.log(interestRate);
+
+let name2 = 'Dexter'; // String Literal
+let age = 7; // Number Literal
+let isApproved = true; // Boolean Literal
+let fstName = undefined;
+let selectedColour = null;
