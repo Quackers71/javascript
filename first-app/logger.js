e6f4be285521c867add8ad6b78606079a82b72a9
quackers71 <24840026+Quackers71@users.noreply.github.com>
2020-04-25 20:53:02 +0100

    Module Wrapper Function

--- first-app/logger.js
+++ first-app/logger.js
@@ -1,3 +1,7 @@
+
+console.log(__filename);
+console.log(__dirname);
+
 var url = "http://mylogger.io/log";
 
 function log(message) {
@@ -6,3 +10,12 @@ function log(message) {
 }
 
 module.exports = log; // module defined as a single function
+module.exports.log = log; // you can also have this
+exports.log = log; // and this
+
+
+// Output
+/* PS C:\Users\fcukq\Desktop\javascript\first-app> node .\app.js
+C:\Users\fcukq\Desktop\javascript\first-app\logger.js
+C:\Users\fcukq\Desktop\javascript\first-app
+Yo, Hello */
