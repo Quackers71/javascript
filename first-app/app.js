014a5ab03acaddd529d746818551d55b76fbe682
quackers71 <24840026+Quackers71@users.noreply.github.com>
2020-04-25 19:28:03 +0100

    Node Modules

--- first-app/app.js
+++ first-app/app.js
@@ -5,3 +5,13 @@ function sayHello(name) {
 
 sayHello('Q')
 //console.log(window);
+
+//console.log(); # global object
+
+setTimeout(1); // call a function after a delay
+clearTimeout();
+setInterval(1);  
+clearInterval(1);
+
+//let message = 'Hello World!';
+//console.log(global.message); // Uncaught ReferenceError: global is not defined
