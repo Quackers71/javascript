5b08b3a6f523980dd2e54156743a790e2d9ae8af
quackers71 <24840026+Quackers71@users.noreply.github.com>
2020-04-26 20:38:18 +0100

    File System Module

--- first-app/app.js
+++ first-app/app.js
@@ -14,3 +14,29 @@ var freeMem = os.freemem();
 
 console.log(`Total Memory: ${totMem}`); // latest formatting without using concatenation
 console.log(`Free Memory: ${freeMem}`);
+
+
+const fs = require('fs');
+
+// const files = fs.readdirSync('./');
+// console.log(files);
+
+fs.readdir('./', function(err, files) {
+  if (err) console.log('Error', err);
+  else console.log('Result', files);
+});
+
+
+
+// Output
+/* PS C:\Users\fcukq\Desktop\javascript\first-app> node .\app.js
+{
+  root: 'C:\\',
+  dir: 'C:\\Users\\fcukq\\Desktop\\javascript\\first-app',
+  base: 'app.js',
+  ext: '.js',
+  name: 'app'
+}
+Total Memory: 8511586304
+Free Memory: 3076952064
+Result [ 'app.js', 'logger.js' ] */
