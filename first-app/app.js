fd1f012742aebbfc9a18c57f8ede90e74e82a6e6
quackers71 <24840026+Quackers71@users.noreply.github.com>
2020-04-26 11:41:52 +0100

    using the os module and method calls

--- first-app/app.js
+++ first-app/app.js
@@ -1,18 +1,16 @@
 
-const path = require('path');
+const path = require('path'); // this will load in the path module and store it in the constant 'path'
 
-var pathObj = path.parse(__filename);
+var pathObj = path.parse(__filename); // this use the parse() method and store the result in 'pathObj'
 
 console.log(pathObj);
 
 
 
-// Output:
-/* PS C:\Users\fcukq\Desktop\javascript\first-app> node .\app.js
-{
-  root: 'C:\\',
-  dir: 'C:\\Users\\fcukq\\Desktop\\javascript\\first-app',
-  base: 'app.js',
-  ext: '.js',
-  name: 'app'
-} */
+const os = require('os'); // load in the os module
+
+var totMem = os.totalmem(); // method call
+var freeMem = os.freemem();
+
+console.log(`Total Memory: ${totMem}`); // latest formatting without using concatenation
+console.log(`Free Memory: ${freeMem}`);
