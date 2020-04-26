5c6f31d464d17b913915cf2a86d432ed09d4c19a
quackers71 <24840026+Quackers71@users.noreply.github.com>
2020-04-26 20:49:16 +0100

    Events Module

--- first-app/app.js
+++ first-app/app.js
@@ -1,42 +1,11 @@
 
-const path = require('path'); // this will load in the path module and store it in the constant 'path'
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
-var pathObj = path.parse(__filename); // this use the parse() method and store the result in 'pathObj'
-
-console.log(pathObj);
-
-
-
-const os = require('os'); // load in the os module
-
-var totMem = os.totalmem(); // method call
-var freeMem = os.freemem();
-
-console.log(`Total Memory: ${totMem}`); // latest formatting without using concatenation
-console.log(`Free Memory: ${freeMem}`);
-
-
-const fs = require('fs');
-
-// const files = fs.readdirSync('./');
-// console.log(files);
-
-fs.readdir('./', function(err, files) {
-  if (err) console.log('Error', err);
-  else console.log('Result', files);
+// Register a listener
+emitter.on('messageLogged', function() {
+  console.log('Listener called');
 });
 
-
-
-// Output
-/* PS C:\Users\fcukq\Desktop\javascript\first-app> node .\app.js
-{
-  root: 'C:\\',
-  dir: 'C:\\Users\\fcukq\\Desktop\\javascript\\first-app',
-  base: 'app.js',
-  ext: '.js',
-  name: 'app'
-}
-Total Memory: 8511586304
-Free Memory: 3076952064
-Result [ 'app.js', 'logger.js' ] */
+// Raise an event
+emitter.emit('messageLogged');  // Making a noise, produce - signalling 
