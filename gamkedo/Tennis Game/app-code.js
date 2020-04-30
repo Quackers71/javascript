d3a4e801a15705d88137080f2be3985aa9b77d68
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-04-30 21:34:01 +0100

    Ball 2D motion & moving mouse paddle

--- gamkedo/Tennis Game/app-code.js
+++ gamkedo/Tennis Game/app-code.js
@@ -5,6 +5,20 @@ var ballY = 50;
 var ballSpeedX = 15;
 var ballSpeedY = 4;
 
+var paddle1Y = 250;
+const PADDLE_HEIGHT = 100;
+
+function calculateMousePos(evt) {
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+    return {
+        x:mouseX,
+        y:mouseY
+    };
+}
+
 window.onload = function() {
 
     canvas = document.getElementById('gameCanvas');
@@ -15,6 +29,12 @@ window.onload = function() {
         moveEverything();
         drawEverything();
     }, 1000/framesPerSecond); // 1000 is millisecond = 1 second
+
+    canvas.addEventListener('mousemove',
+        function(evt) {
+            var mousePos = calculateMousePos(evt);
+            paddle1Y = mousePos.y - (PADDLE_HEIGHT/2);
+        });
 }
 
 function moveEverything(){
@@ -40,7 +60,7 @@ function drawEverything() {
     colourRect(0,0,canvas.width, canvas.height,'black');
 
     // left player paddle
-    colourRect(0,210,10,100,'white');
+    colourRect(0,paddle1Y,10,PADDLE_HEIGHT,'white');
 
     // draws the ball
     colourCircle(ballX, ballY, 10, 'white');
