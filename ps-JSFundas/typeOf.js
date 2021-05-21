c324f80f12195ae5e067c399df91e40c42410883
Quackers <robq71@gmail.com>
2021-05-21 08:24:48 +0100

    Section03 - typeof()

---
+++ ps-JSFundas/typeOf.js
@@ -0,0 +1,8 @@
+typeof(1); // number
+typeof(true); // boolean
+typeof('Hello'); // string
+typeof( function(){} ); // function
+typeof({}); // object
+typeof(null); // object
+typeof(undefined); // undefined
+typeof(NaN); // number
