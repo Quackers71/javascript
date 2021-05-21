64343340a7ebda6ea0e088f0e7c8b2a9e4c48d31
Quackers <robq71@gmail.com>
2021-05-21 18:42:53 +0100

    Section04 - Equality Operators

---
+++ ps-JSFundas/Operators/equalityOperators.js
@@ -0,0 +1,20 @@
+
+console.log(1 == 1);
+console.log(1 == true);
+console.log(1 === 1);
+console.log(1 === true);
+
+let id = 123;
+console.log(id === "123");
+console.log(id == "123");
+console.log(id === 123);
+console.log(id != "123");
+console.log(id !== "123");
+
+
+/* 
+if (var1 == var2) { }
+if (var1 === var2) { }
+if (var1 != var2) { }
+if (var1 !== var2) { } 
+*/
