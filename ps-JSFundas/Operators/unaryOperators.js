48a63afbad0590dc91afcb9b1e950d3de311c046
Quackers <robq71@gmail.com>
2021-05-21 21:07:39 +0100

    Section04 - Unary Operators

---
+++ ps-JSFundas/Operators/unaryOperators.js
@@ -0,0 +1,20 @@
+
+let year = 1967;
+console.log(year++);
+console.log(++year);
+console.log(--year);
+console.log(--year);
+console.log(year);
+
+let nextYear = "-1971";
+console.log(typeof(nextYear));
+console.log(typeof(-nextYear));
+console.log(+nextYear);
+console.log(-nextYear);
+
+/* ++var1;
+var1++;
+--var1;
++var1 */
+
+
