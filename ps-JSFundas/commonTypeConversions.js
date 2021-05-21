67504193bb5298bc91e4a1392e328435f1f61793
Quackers <robq71@gmail.com>
2021-05-21 17:34:25 +0100

    Section03 - Common Type Conversions

---
+++ ps-JSFundas/commonTypeConversions.js
@@ -0,0 +1,15 @@
+
+console.log( Number.parseFloat('55.88ABC'));
+console.log( Number.parseFloat('A55.88BC'));
+console.log( Number.parseInt('55ABC'));
+console.log( typeof(Number.parseInt('55')));
+
+/* // convert to string
+foo.toString();
+
+// convert string to integer
+Number.parseInt('55');
+
+// convert string to number
+Number.parseFloat('55.99'); */
+
