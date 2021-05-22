821e11d81318a0c339d8881ef2fd553516afa948
Quackers <robq71@gmail.com>
2021-05-22 08:28:58 +0100

    Section04 - Assignment Operators

---
+++ ps-JSFundas/Operators/assignmentOperators.js
@@ -0,0 +1,23 @@
+
+let year = 1971;
+year += 10;
+console.log( year );
+year -= 10;
+console.log( year );
+year >>= 1;
+console.log( year );
+year <<= 1;
+console.log( year );
+
+
+/* 
+var1 += 10;
+var1 -= 10;
+var1 /= 10;
+var1 *= 10;
+var1 %= 10;
+var1 <<= 1;
+var1 >>= 1;
+var1 <<= 1;
+var1 >>>= 1; 
+*/
