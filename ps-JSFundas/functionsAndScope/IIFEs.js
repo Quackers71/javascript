54bfb7a373e5753c8ab0004c215b439241e256a8
Quackers <robq71@gmail.com>
2021-05-22 14:41:11 +0100

    Section05 - Immediately Invoked Function Expressions

---
+++ ps-JSFundas/functionsAndScope/IIFEs.js
@@ -0,0 +1,14 @@
+
+let app = (function(){
+    let carId = 123;
+    console.log('in function');
+    return { };
+})();
+
+console.log(app);
+
+
+/* (function(){
+    console.log('in function');
+})(); 
+*/
