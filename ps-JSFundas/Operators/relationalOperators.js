2e8b245fe983d12b8187dc6089f791d427e654ae
Quackers <robq71@gmail.com>
2021-05-22 08:16:40 +0100

    Section04 - Relational Operators

---
+++ ps-JSFundas/Operators/relationalOperators.js
@@ -0,0 +1,15 @@
+
+let s1 = 'Zoo';
+let s2 = 'alphabet';
+
+//if (s1 < s2)
+if (s1.toUpperCase() < s2.toUpperCase()){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+
+// > < >= <=
+// "Zoo" < "alphabet" / true because Uppercase characters come before Lowercase
