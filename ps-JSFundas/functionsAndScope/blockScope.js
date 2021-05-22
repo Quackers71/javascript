b157372fde95e99be0361f41d30a3c8bab3beb93
Quackers <robq71@gmail.com>
2021-05-22 14:34:48 +0100

    Section05 - Block Scope

---
+++ ps-JSFundas/functionsAndScope/blockScope.js
@@ -0,0 +1,17 @@
+
+
+
+let message = 'Outside';
+if ( 5 === 5){
+    let message = 'Equal';
+    console.log(message);    
+}
+
+console.log(message);
+
+
+/* if ( 5 === 5){
+    let message = 'Equal';  
+}
+console.log(message); // Error: message is not defined
+*/
