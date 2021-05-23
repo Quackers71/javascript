e59ffbed00cf56f5b186f0fa1decfe3988ad59eb
Quackers <robq71@gmail.com>
2021-05-23 12:38:48 +0100

    Section07 - Creating a Module

--- ps-JSFundas/src/classesAndModules/creatingAModule.js
+++ ps-JSFundas/src/classesAndModules/creatingAModule.js
@@ -1,10 +1,11 @@
 
 
-
-class Car {
+// cut and pasted into car.js
+/* class Car {
     constructor(id){
         this.id = id;
     }
-}
+} */
+
 let car = new Car(123);
 console.log( car.id ); // 123
---
+++ ps-JSFundas/src/scripts/index.js
@@ -0,0 +1,6 @@
+import car
+
+
+
+let car = new Car(123);
+console.log( car.id ); // 123
