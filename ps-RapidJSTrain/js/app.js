e91ea396dc20d9829cf0ff9cffe8f8c2a10dafdf
Quackers <robq71@gmail.com>
2021-05-28 20:45:36 +0100

    Section2 - Variables and Types

--- ps-RapidJSTrain/js/app.js
+++ ps-RapidJSTrain/js/app.js
@@ -1,5 +1,36 @@
 
 
 
+/* var orderId = "ORD-9001";
+orderId = 1201;
+console.log(orderId); // 1201
 
-console.log('in app.js');
+var orderId;
+console.log(orderId); // undefined
+*/
+
+/* 'use strict';
+orderId = 9001;
+console.log(orderId); // ReferenceError: orderId3 is not defined 
+*/
+
+/* var orderId = 9001.01;
+console.log(typeof orderId); // number 
+*/
+
+var order = {
+    orderId: 9001,
+    isActive: true
+};
+console.log(typeof order); // object
+
+var cancelledOrders = [9001, 9002, 9003];
+console.log(typeof cancelledOrders); // object
+
+var order = null;
+console.log(typeof order); // object
+
+function cancelOrder(orderId){
+
+};
+console.log(typeof cancelOrder); // function
