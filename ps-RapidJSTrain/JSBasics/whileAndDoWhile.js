634635a046895bd560e33fa459871d992ef400d7
Quackers <robq71@gmail.com>
2021-05-29 10:34:17 +0100

    Section2 - while and do...while

---
+++ ps-RapidJSTrain/JSBasics/whileAndDoWhile.js
@@ -0,0 +1,33 @@
+
+
+
+/* var LineItemCount = 10;
+var currentItem = 1;
+do {
+    console.log("item: " + currentItem);
+    currentItem++;
+} while (currentItem < LineItemCount+1); // 1,2,3...10 
+*/
+    
+
+/* var LineItemCount = 11;
+var currentItem = 1;
+while (currentItem < LineItemCount)
+    console.log("item: " + currentItem); // loops forever :-o 
+*/
+
+
+/* var LineItemCount = 11;
+var currentItem = 1;
+while (currentItem < LineItemCount)
+    console.log("item: " + currentItem++); // 1,2,3...10 
+*/
+
+
+/* var LineItemCount = 11;
+var currentItem = 1;
+while (currentItem < LineItemCount){
+    console.log("item: " + currentItem);
+    currentItem++;
+} // 1,2,3...10 
+*/
--- ps-RapidJSTrain/js/app.js
+++ ps-RapidJSTrain/js/app.js
@@ -1,21 +1,9 @@
 
 
 
-var orderTotal = 99.99;
-var discount;
-
-switch (true) {
-    case orderTotal >= 50 && orderTotal < 75:
-        discount = 10;
-        break;
-    case orderTotal >= 75 && orderTotal < 100:
-        discount = 20;
-        break;
-    case orderTotal >= 100:
-        discount = 30;
-        break;
-    default:
-        discont = 0;
-}
-
-console.log(discount); // 20
+var LineItemCount = 10;
+var currentItem = undefined;
+do {
+    console.log("item: " + currentItem);
+    currentItem++;
+} while (currentItem < LineItemCount+1); // undefined
