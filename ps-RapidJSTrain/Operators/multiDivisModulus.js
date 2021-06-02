13ed7b040a9f6ef44f97bf8c0700b6ea8874606b
Quackers <robq71@gmail.com>
2021-06-02 22:02:01 +0100

    Section4 - Multiplication, Division & Modulus

---
+++ ps-RapidJSTrain/Operators/multiDivisModulus.js
@@ -0,0 +1,88 @@
+
+
+
+var total = 9 % "   4   ";
+console.log(total); // 1
+
+
+/* var total = 9 / "3";
+console.log(total); // 3 
+*/
+
+
+/* var total = -9 / 0;
+console.log(total); // -Infinity 
+*/
+
+
+/* var total = 9 / 0;
+console.log(total); // Infinity 
+*/
+
+
+/* var total = -4 * "XYZ";
+console.log(total); // NaN 
+*/
+
+
+/* var total = -4 * Infinity;
+console.log(total); // -Infinity 
+*/
+
+
+/* var total = 4 * Infinity;
+console.log(total); // Infinity 
+*/
+
+
+/* var obj = {
+    valueOf: function(){ return 100; }
+};
+var total = 4 * obj;
+console.log(total); // 400 
+*/
+
+
+/* var total = 20 * null; // null = 0
+console.log(total); // 0 
+*/
+
+
+/* var total = 20 * undefined;
+console.log(total); // NaN 
+*/
+
+
+/* var total = 20 * NaN;
+console.log(total); // NaN */
+
+
+// another way!
+/* var total = 1.1 * 1.1;
+console.log(total);
+if (total.toFixed(2) == 1.21){
+    console.log('1.21 == ' + total.toFixed(2) + ' so it\'s Fixed!');
+}
+else{
+    console.log('oh no!');
+} */
+
+
+/* var total = 1.1 * 1.1;
+console.log(total.toFixed(2)); // 1.21
+if (total != 1.21){
+    console.log('Big Problem!');
+}
+else{
+    console.log('Bang on!'); // true
+} */
+
+
+/* var total = 1.1 * 1.1;
+console.log(total.toFixed(2)); // 1.21 
+*/
+
+
+/* var total = 9 * 3;
+console.log(total); // 27 
+*/
--- ps-RapidJSTrain/js/app.js
+++ ps-RapidJSTrain/js/app.js
@@ -1,8 +1,12 @@
 
 
 
-var obj = {
-    valueOf: function(){ return 100; }
-};
-var total = 300 - obj;
+// another way!
+var total = 1.1 * 1.1;
 console.log(total);
+if (total.toFixed(2) == 1.21){
+    console.log('1.21 == ' + total.toFixed(2) + ' so it\'s now Fixed!');
+}
+else{
+    console.log('oh no!'); 
+}
