520083d416744903651b36ff34c97b7b2d0dfd31
Quackers <robq71@gmail.com>
2021-06-09 21:11:01 +0100

    Section4 - Relational Operators

--- ps-RapidJSTrain/js/app.js
+++ ps-RapidJSTrain/js/app.js
@@ -1,13 +1,38 @@
 
 
 
-if (55 !== 55) // false, as they're identical
+var total = NaN; // output = (nothing)
+if (total < 5)
+    console.log('less than 5');
+if (total <= 5)
+    console.log('greater than or equal to 5');
+
+
+if ('42' < '142') // false - compares as strings so 1 comes before 4
     console.log('true'); 
 else
     console.log('false');
 
 
-if (null === undefined) // false, however true with ==
+if ('42' < 55) // true - JS will convert string to number
+    console.log('true');
+else
+    console.log('false');
+
+
+if ("Beta".toLowerCase() < "alpha".toLowerCase()) // false
     console.log('true'); 
+else
+    console.log('false');
+
+
+if ("Beta" < "alpha") // true
+    console.log('true'); 
+else
+    console.log('false');
+
+
+if ("beta" < "Alpha") // false - ascii or unicode values
+    console.log('true'); //      Capitals come before Lowercase
 else
     console.log('false');
