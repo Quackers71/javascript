27190a5e25bceb72bb9f4becbde73e5b6582e88f
Quackers <robq71@gmail.com>
2021-06-15 20:57:22 +0100

    Section7 - Recursion

---
+++ ps-RapidJSTrain/Functions/recursion.js
@@ -0,0 +1,91 @@
+
+
+
+var orgChart = {
+    name: 'Michael', subordinates: [
+        {
+            name: 'Andy', subordinates: [
+                {
+                    name: 'Dwight', subordinates: []
+                },
+                {
+                    name: 'Kevin', subordinates: []
+                }
+            ]
+        }
+    ]
+};
+
+var fn = function showAllEmployees(topEmployee) {
+    console.log(topEmployee.name);
+    for (var i = 0; i < topEmployee.subordinates.length; i++)
+        showAllEmployees(topEmployee.subordinates[i]);
+};
+
+var fn2 = fn;
+fn = null;
+fn2(orgChart);
+
+/* Michael
+Andy
+Dwight
+Kevin */
+
+
+var orgChart = {
+    name: 'Michael', subordinates: [
+        {
+            name: 'Andy', subordinates: [
+                {
+                    name: 'Dwight', subordinates: []
+                },
+                {
+                    name: 'Kevin', subordinates: []
+                }
+            ]
+        }
+    ]
+};
+
+var fn = function(topEmployee) {
+    console.log(topEmployee.name);
+    for (var i = 0; i < topEmployee.subordinates.length; i++)
+        fn(topEmployee.subordinates[i]);
+};
+
+var fn2 = fn;
+fn = null;
+fn2(orgChart);
+
+/* Michael
+Uncaught TypeError: fn is not a function
+    at fn  */
+
+
+var orgChart = {
+    name: 'Michael', subordinates: [
+        {
+            name: 'Andy', subordinates: [
+                {
+                    name: 'Dwight', subordinates: []
+                },
+                {
+                    name: 'Kevin', subordinates: []
+                }
+            ]
+        }
+    ]
+};
+
+var fn = function(topEmployee) {
+    console.log(topEmployee.name);
+    for (var i = 0; i < topEmployee.subordinates.length; i++)
+        fn(topEmployee.subordinates[i]);
+};
+
+fn(orgChart);
+
+/* Michael
+Andy
+Dwight
+Kevin */
--- ps-RapidJSTrain/js/app.js
+++ ps-RapidJSTrain/js/app.js
@@ -1,9 +1,32 @@
 
 
 
-+function() {
-    var employeeName = 'Jill';
-    console.log('Executed!');
-}();
-console.log(employeeName); // Executed!
-// Uncaught ReferenceError: employeeName is not defined
+var orgChart = {
+    name: 'Michael', subordinates: [
+        {
+            name: 'Andy', subordinates: [
+                {
+                    name: 'Dwight', subordinates: []
+                },
+                {
+                    name: 'Kevin', subordinates: []
+                }
+            ]
+        }
+    ]
+};
+
+var fn = function showAllEmployees(topEmployee) {
+    console.log(topEmployee.name);
+    for (var i = 0; i < topEmployee.subordinates.length; i++)
+        showAllEmployees(topEmployee.subordinates[i]);
+};
+
+var fn2 = fn;
+fn = null;
+fn2(orgChart);
+
+/* Michael
+Andy
+Dwight
+Kevin */
