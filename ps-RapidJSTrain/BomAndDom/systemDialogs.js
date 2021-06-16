804b762eed25ab7e5d29324eaccf3f67e8acb511
Quackers <robq71@gmail.com>
2021-06-16 21:48:05 +0100

    Section8 - System Dialogs

---
+++ ps-RapidJSTrain/BomAndDom/systemDialogs.js
@@ -0,0 +1,17 @@
+
+
+
+var result = prompt('Your name?');
+console.log(result);
+// null, or whatever was entered in the prompt
+
+
+if (confirm('Delete EVERYTHING?!')) {
+    console.log('You asked for it!');
+}
+else {
+    console.log('Maybe next time...');
+}
+
+
+alert("Hello World!");
--- ps-RapidJSTrain/js/app.js
+++ ps-RapidJSTrain/js/app.js
@@ -1,17 +1,6 @@
 
 
 
-console.log(new Date().getSeconds());
-var id = setInterval(function() {
-    var secs = new Date().getSeconds();
-    console.log(secs);
-    if (secs === 10)
-        clearInterval(id);
-}, 1000);
-// 4
-// 5
-// 6
-// 7
-// 8
-// 9
-// 10
+var result = prompt('Your name?');
+console.log(result);
+// null, or whatever was entered in the prompt
