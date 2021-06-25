04a41a95d3cb2aef4f51f2fa4944af65cc4bb2c5
Quackers <robq71@gmail.com>
2021-06-25 23:29:28 +0100

    Scope and Execution Example

--- AdvancedJavaScript/Scope.js
+++ AdvancedJavaScript/Scope.js
@@ -9,32 +9,41 @@ function bar() {
 //      LHS    RHS
 //    target  source
     var foo = "baz";
-};
 
-function baz(foo) {
-//  LHS    RHS
-    foo = "bam";
-    bam = "yay";
+    function baz(foo) {
+    //  LHS    RHS
+        foo = "bam";
+        bam = "yay";
+    }
+    baz(42);
 };
-baz();
 
 console.log(foo);  // bar
-console.log(bar);
+console.log(bar); // output below
 /* ƒ bar() {
-    var foo = "baz";
-} */
-//console.log(bam);
-// Uncaught ReferenceError: bam is not define
+    //      LHS    RHS
+    //    target  source
+        var foo = "baz";
+    
+        function baz(foo) {
+        //  LHS    RHS
+            foo = "bam";
+            bam = "yay";
+        }
+        baz();
+    } */
 
-bar();
-foo; // "bar"
-bam; // "yay"
-baz(); // Error!
+
+//bar(); // RHS
+//foo; // "bar"
+//bam; // "yay"
+//baz(); // Error!
 
 console.log("This is bar() it is " + bar() + " but is a type of :" + typeof(bar)); // undefined
 console.log('The variable FOO is a ' + typeof(foo) + " which is equal to " + '"%s"', foo);
 console.log('The variable BAM is a ' + typeof(bam) + " which is equal to " + '"%s"', bam);
 console.log("This is baz() it is " + baz() + " but is a type of :" + typeof(baz)); // undefined
+// Uncaught ReferenceError: baz is not defined
 
 const style = 'font-weight: bold'
 console.log('\n%cbam', style);
