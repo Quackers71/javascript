b3112eca62aa4b7bbd72604119f2ccc3a3a5cd1a
Quackers <robq71@gmail.com>
2021-06-26 10:11:03 +0100

    Scope and Execution Example Cont.

--- AdvancedJavaScript/Scope.js
+++ AdvancedJavaScript/Scope.js
@@ -35,15 +35,21 @@ console.log(bar); // output below
 
 
 //bar(); // RHS
-//foo; // "bar"
-//bam; // "yay"
-//baz(); // Error!
+//foo; // "bar" - Global Scope RHS
+//bam; // "yay" - Global Scope RHS - GS created him
+//baz(); // Error! GS RHS value = Nope!
 
 console.log("This is bar() it is " + bar() + " but is a type of :" + typeof(bar)); // undefined
 console.log('The variable FOO is a ' + typeof(foo) + " which is equal to " + '"%s"', foo);
 console.log('The variable BAM is a ' + typeof(bam) + " which is equal to " + '"%s"', bam);
 console.log("This is baz() it is " + baz() + " but is a type of :" + typeof(baz)); // undefined
 // Uncaught ReferenceError: baz is not defined
+
+/* In Non-strict mode, reference errors result in unfulfilled
+or undeclared RHS references.  Automatic Globals esult in unfulfilled
+or undeclared LHS references.
+In strict mode, they both result in reference errors...
+*/
 
 const style = 'font-weight: bold'
 console.log('\n%cbam', style);
