e215e6c1a13f9a4fbcbce8256091257c4a301fdd
Quackers <robq71@gmail.com>
2021-06-27 18:19:51 +0100

    IIFE Pattern cont.

--- AdvancedJavaScript/IIFE.js
+++ AdvancedJavaScript/IIFE.js
@@ -5,6 +5,27 @@ var foo = "foo";
 
 (function(bar) {
     var foo = bar;
+    console.log(foo);
+})(foo); // })(foo); - Immediately executing the value of the expression stack.
+
+console.log(foo);
+
+
+var foo = "foo";
+
+(function(bar) {
+    var foo = bar;
+    console.log(foo);
+}(foo)); // }(foo)); - an alternative Stylistic choice
+
+console.log(foo);
+
+
+
+var foo = "foo";
+
+(function(bar) {
+    var foo = bar;
     console.log(foo); // "foo"
 })(foo);
 
--- AdvancedJavaScript/js/app.js
+++ AdvancedJavaScript/js/app.js
@@ -5,7 +5,17 @@ var foo = "foo";
 
 (function(bar) {
     var foo = bar;
-    console.log(foo); // "foo"
-})(foo);
+    console.log(foo);
+})(foo); // })(foo); - Immediately executing the value of the expression stack.
 
-console.log(foo); // "foo"
+console.log(foo);
+
+
+var foo = "foo";
+
+(function(bar) {
+    var foo = bar;
+    console.log(foo);
+}(foo)); // }(foo)); - an alternative Stylistic choice
+
+console.log(foo);
