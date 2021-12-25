21bdf3b7e2b9cc90acb58d6b77ad51998c4067f9
Quackers <robq71@gmail.com>
2021-12-25 21:51:34 +0000

    99 bottles of beer

---
+++ head1st-js/my_files/index copy1.js
@@ -0,0 +1,12 @@
+
+var i = 0;
+while (i < 2) {
+    document.write("Happy Birthday to you.<br>");
+    i = i + 1;
+}
+var name = "Joe";
+document.write("Happy Birthday dear " + name + ",<br>");
+document.write("Happy Birthday to you.<br>");
+
+var message = "Yo Yo" + " " +"Sicko!";
+console.log(message);
