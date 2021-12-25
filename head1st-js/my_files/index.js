dba0962181a6126627cad01015dcc9b512a0033b
Quackers <robq71@gmail.com>
2021-12-25 21:28:57 +0000

    moved js to file

---
+++ head1st-js/my_files/index.js
@@ -0,0 +1,9 @@
+
+var i = 0;
+while (i < 2) {
+    document.write("Happy Birthday to you.<br>");
+    i = i + 1;
+}
+var name = "Joe";
+document.write("Happy Birthday dear " + name + ",<br>");
+document.write("Happy Birthday to you.<br>");
