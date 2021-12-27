a3faa74d3bdc31853ff9f877f94ba87516113810
Quackers <robq71@gmail.com>
2021-12-27 13:53:21 +0000

    added a little more to the console output

--- head1st-js/my_files/index.js
+++ head1st-js/my_files/index.js
@@ -1,15 +1,20 @@
 
 var word = "bottles";
+var word2 = "bottle";
 var count = 99;
-while (count > 0) {
+while (count > 1) {
     console.log(count + " " + word + " of beer on the wall");
     console.log(count + " " + word + " of beer,");
-    console.log("Take one down, pass it around");
+    console.log("Take one down, pass it around,\n\n");
     count = count - 1;
 
-    if (count > 0) {
-        console.log(count + " " + word + " of beer on the wall.");
-    } else {
+    if (count == 1) {
+        console.log(count + " " + word2 + " of beer on the wall.");
+        console.log(count + " " + word2 + " of beer,");
+        console.log("Take one down, pass it around,\n\n");
+        count = count - 1;
+    }
+    if (count < 1) {
         console.log("No more " + word + " of beer on the wall.");
     }
 }
