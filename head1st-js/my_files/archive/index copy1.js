f30e42c22e40b80b3744e7ca635b0bc7942040a7
Quackers <robq71@gmail.com>
2021-12-28 11:53:48 +0000

    tidy up

--- head1st-js/my_files/archive/index copy1.js
+++ head1st-js/my_files/archive/index copy1.js
@@ -1,10 +1,9 @@
-
+var name = "Joe";
 var i = 0;
 while (i < 2) {
     document.write("Happy Birthday to you.<br>");
     i = i + 1;
 }
-var name = "Joe";
 document.write("Happy Birthday dear " + name + ",<br>");
 document.write("Happy Birthday to you.<br>");
 
