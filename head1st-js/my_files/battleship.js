482a14dcb00bc31568ea8a0123ad5a65b539b651
Quackers <robq71@gmail.com>
2021-12-30 13:30:20 +0000

    randomized the battleship placement

--- head1st-js/my_files/battleship.js
+++ head1st-js/my_files/battleship.js
@@ -1,6 +1,7 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 
 var guess;
 var hits = 0;
