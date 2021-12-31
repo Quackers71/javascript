2c8bbc261378dd13cb4b12da577ec744731ac061
Quackers <robq71@gmail.com>
2021-12-31 11:42:11 +0000

    pre-function

---
+++ head1st-js/my_files/archive/index copy3.js
@@ -0,0 +1,28 @@
+var dogName = "Rover";
+var dogWeight = 23;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF!");
+} else {
+    console.log(dogName + " says woof woof");
+}
+var dogName = "Spot";
+var dogWeight = 13;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF!");
+} else {
+    console.log(dogName + " says woof woof");
+}
+var dogName = "Spike";
+var dogWeight = 53;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF!");
+} else {
+    console.log(dogName + " says woof woof");
+}
+var dogName = "Lady";
+var dogWeight = 17;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF!");
+} else {
+    console.log(dogName + " says woof woof");
+}
