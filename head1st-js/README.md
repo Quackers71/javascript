e91552349881d4717a459179300a244397e660d4
Quackers <robq71@gmail.com>
2022-01-07 21:51:37 +0000

    1st attempt at Bubbles-R-Us

--- head1st-js/README.md
+++ head1st-js/README.md
@@ -10,3 +10,7 @@
 - When you call a function you pass it arguments and those arguments then get matched up with the parameters in the function definition.
 - You can also pass variables as arguments, and that's often the more common case.  See my_files/archive/index_copy6.js for examples.<br/><br/>
 ![](./AnatomyofaFunction.png)
+
+https://www.freecodecamp.org/news/three-ways-to-return-largest-numbers-in-arrays-in-javascript-5d977baa80a1/
+
+https://www.codegrepper.com/search.php?answer_removed=1&q=-=%20javascript
--- head1st-js/my_files/index.js
+++ head1st-js/my_files/index.js
@@ -1,13 +1,36 @@
-var words1 = ["24/7", "multi-tier", "30,000 foot", "B-to-B", "win-win"];
-var words2 = ["empowered", "value-added", "oriented", "focused", "aligned"];
-var words3 = ["process", "solution", "tipping-point", "strategy", "vision"];
+var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54,
+ 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61,
+ 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 
-var rand1 = Math.floor(Math.random() * words1.length);
-var rand2 = Math.floor(Math.random() * words2.length);
-var rand3 = Math.floor(Math.random() * words3.length);
 
-var phrase = words1[rand1] + "    " + words2[rand2] + "   " + words3[rand3];
+function outputScores(list) {
+   var index = scores.length;
+   for (var i = 0; i < index; i++) {
+       console.log("Bubble solution #" + i + "   score: " + scores[i]);
+   }
+}
 
-console.log("    " + rand1 + "          " + rand2 + "         " + rand3);
+outputScores(scores);
 
-console.log(phrase);
+console.log("Bubbles tests: " + scores.length);
+
+console.log("Highest Bubble score : " + Math.max.apply(null, scores));
+
+function indexOfMax(arr) {
+    if (arr.length === 0) {
+        return -1;
+    }
+
+    var max = arr[0];
+    var maxIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            maxIndex = i;
+            max = arr[i];
+        }
+    }
+    return maxIndex;
+}
+
+console.log("Solution with highest score : #" + indexOfMax(scores));
