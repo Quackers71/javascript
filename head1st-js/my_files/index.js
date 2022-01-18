25351d04913bfe099c308b32f55d383350e65bf6
Quackers <robq71@gmail.com>
2022-01-18 21:56:55 +0000

    Understanding Objects

--- head1st-js/my_files/index.js
+++ head1st-js/my_files/index.js
@@ -1,62 +1,64 @@
-'use strict';
-var scores = [60, 50, 60, 58, 54, 54,
-              58, 50, 52, 54, 48, 69,
-              34, 55, 51, 52, 44, 51,
-              69, 64, 66, 55, 52, 61,
-              46, 31, 57, 52, 44, 18,
-              41, 53, 55, 61, 51, 44];
+var chevy = {
+    make: "Chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "red",
+    passengers: 2,
+    convertible: false,
+    mileage: 1021,
+    transmission: "Auto",
+    registration: "0MG"
+};
 
-var costs = [.25, .27, .25, .25, .25, .25,
-             .33, .31, .25, .29, .27, .22,
-             .31, .25, .25, .33, .21, .25,
-             .25, .25, .28, .25, .24, .22,
-             .20, .25, .30, .25, .24, .25,
-             .25, .25, .27, .25, .26, .29];
+var cadi = {
+    make: "General Motors",
+    model: "Cadillac",
+    year: 1955,
+    color: "tan",
+    passengers: 5,
+    convertible: false,
+    mileage: 12892,
+    transmission: "Auto",
+    registration: "D00M3R"
+};
 
-function printAndGetHighScore(scores) {
-    var highScore = 0;
-    for (var i = 0; i < scores.length; i++) {
-        console.log("Bubble solution #" + i + "   score: " + scores[i] + " cost : " + costs[i]);
-        if (scores[i] > highScore) {
-            highScore = scores[i];
-        }
-    }
-    return highScore;
-}
-        
-function getBestResults(scores, highScore) {
-    var bestSolutions = [];
-    for (var i = 0; i < scores.length; i++) {
-        if (scores[i] == highScore) {
-            bestSolutions.push("#"+i+" ");
-        }
-    }
-    return bestSolutions;
-}
+var fiat = {
+    make: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "Medium Blue",
+    passengers: 2,
+    convertible: false,
+    mileage: 88000,
+    transmission: "Manual",
+    registration: "N99WEB"
+};
 
-function getMostCostEffectiveSolution(scores, costs, highscore) {
-    var cost = 100;
-    var index;
-    for (var i = 0; i < scores.length; i++) {
-        if (scores[i] == highscore) {
-            if (cost > costs[i]) {
-                console.log("initialised cost = " + cost);
-                index = i;
-                console.log("position[i] = " + i);
-                cost = costs[i];
-                console.log("lowest high scoring cost = " + cost);
-            }
-        }
-    }
-    return index;
+var miles = fiat.mileage;
+var colors = fiat.color;
+if (miles > 2000 || colors == "Med Blue") {
+    console.log(JSON.stringify(fiat, null, 2));
 }
 
-var highScore = printAndGetHighScore(scores);
-console.log("Bubbles tests: " + scores.length);
-console.log("Highest Bubble score : " + highScore);
+fiat.mileage = 10000;
+fiat.needsWashing = true;
 
-var bestSolutions = getBestResults(scores, highScore);
-console.log("Solutions with the highest score : " + bestSolutions);
+console.log("mileage Changed");
+console.log("needsWashing added");
+console.table(fiat);
 
-var mostCostEffective = getMostCostEffectiveSolution(scores, costs, highScore);
-console.log("Bubble Solution #" + mostCostEffective + " is the most cost effective");
+if (fiat.year < 1965) {
+    console.log("This " + fiat.make + " " + fiat.model + ", is a Classic Car...");
+    classic = true;
+}
+
+var people = fiat.passengers;
+for (var i = 0; i < people; i++) {
+    console.log("Yo!");
+    addPersonToCar = true;
+}
+console.log("There are " + people + " people in this car.");
+
+if (people == i) {
+    console.log("were " + people + " people were added to this car? = " + addPersonToCar);
+}
