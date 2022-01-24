6d1139cedca519d6159c20b62d0e990f97f3cd4e
Quackers <robq71@gmail.com>
2022-01-24 23:01:40 +0000

    clean up

--- head1st-js/my_files/archive/#37-addTextFunc.js
+++ head1st-js/my_files/archive/#37-addTextFunc.js
@@ -1,28 +1,3 @@
-/* var access = document.getElementById("code3");
-var code = access.innerHTML;
-text = "Midnight";
-code = code + text;
-alert(code);
-
-var insertText = document.createTextNode(text);
-access.appendChild(insertText);
-console.log(text);
-
-access.style.color = 'pink';
-
-var access = document.getElementById("code2");
-text = "M";
-var insertText = document.createTextNode(text);
-access.appendChild(insertText);
-access.style.color = 'purple';
-
-var access = document.getElementById("code1");
-text = "Gutter";
-var insertText = document.createTextNode(text);
-access.appendChild(insertText);
-access.style.color = 'beige'; */
-
-
 function addText(id, text) {
     var text;
     var access = document.getElementById(id);
