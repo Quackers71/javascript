531bdf336817125b8e45f100342e5fad4653b7da
Quackers <robq71@gmail.com>
2022-01-24 20:59:39 +0000

    appendChild within a function

--- head1st-js/my_files/index.js
+++ head1st-js/my_files/index.js
@@ -1,14 +1,35 @@
-var access = document.getElementById("code3");
-// var code = access.innerHTML;
+/* var access = document.getElementById("code3");
+var code = access.innerHTML;
 text = "Midnight";
-// code = code + text;
-// alert(code);
+code = code + text;
+alert(code);
 
 var insertText = document.createTextNode(text);
 access.appendChild(insertText);
 console.log(text);
 
-// text.style.color = "white";
+access.style.color = 'pink';
 
-// insertText.style.color = "green";
-// access.style.color = 'purple';
+var access = document.getElementById("code2");
+text = "M";
+var insertText = document.createTextNode(text);
+access.appendChild(insertText);
+access.style.color = 'purple';
+
+var access = document.getElementById("code1");
+text = "Gutter";
+var insertText = document.createTextNode(text);
+access.appendChild(insertText);
+access.style.color = 'beige'; */
+
+
+function addText(id, text) {
+    var text;
+    var access = document.getElementById(id);
+    var insertText = document.createTextNode(text);
+    access.appendChild(insertText);
+}
+
+addText("code1", "Gutter");
+addText("code2", "M");
+addText("code3", "Midnight");
