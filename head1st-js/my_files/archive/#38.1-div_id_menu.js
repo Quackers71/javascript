d50feb7839c7c1f252dbbaac9610daff51a3eed2
Quackers <robq71@gmail.com>
2022-01-29 19:44:53 +0000

    updated with div id menu files from previous task

---
+++ head1st-js/my_files/archive/#38.1-div_id_menu.js
@@ -0,0 +1,7 @@
+var planet = document.getElementById("greenplanet");
+console.log(planet.innerHTML);
+planet.innerHTML = "Red Alert: hit by phaser fire!";
+console.log(planet.innerHTML);
+
+var myMenu = document.getElementById("menu");
+console.log(myMenu.innerHTML);
