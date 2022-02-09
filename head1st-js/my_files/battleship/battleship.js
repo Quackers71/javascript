94e29f77187e1860f43d34c3fbfb7ed9723d5b2b
Quackers <robq71@gmail.com>
2022-02-09 21:01:18 +0000

    model object implemented

--- head1st-js/my_files/battleship/battleship.js
+++ head1st-js/my_files/battleship/battleship.js
@@ -20,5 +20,33 @@ var model = {
     shipsSunk: 0,
     ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
             { locations: ["24", "34", "44"], hits: ["", "", ""] },
-            { locations: ["10", "11", "12"], hits: ["", "", ""] }]
+            { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+    fire: function(guess) {
+
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            var index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sank my battleship!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
+    isSunk: function(ship) {
+        for (var i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    }
 };
