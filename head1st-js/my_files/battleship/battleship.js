2e4dabe89e163a8bd50be51cbbd6b10bb292dddc
Quackers <robq71@gmail.com>
2022-02-09 23:12:27 +0000

    calling the model's fire method

--- head1st-js/my_files/battleship/battleship.js
+++ head1st-js/my_files/battleship/battleship.js
@@ -50,3 +50,14 @@ var model = {
         return true;
     }
 };
+
+model.fire("53");
+model.fire("06");
+model.fire("16");
+model.fire("26");
+model.fire("34");
+model.fire("24");
+model.fire("44");
+model.fire("12");
+model.fire("11");
+model.fire("10");
