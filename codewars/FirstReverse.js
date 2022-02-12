653cbcf6c131842065b434f77a1732686313afcc
Quackers <robq71@gmail.com>
2022-02-12 17:39:30 +0000

    coderbyte JS reverse challengegs

---
+++ codewars/FirstReverse.js
@@ -0,0 +1,18 @@
+function FirstReverse(str) { 
+    console.log(str);
+    for (var a = 0; a < str.length; a++) {
+        console.log(a+" "+str[a]);
+    }
+    console.log("\r");
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        console.log(i+" "+str[i]);
+        newString = newString + str[i];
+    }
+    return newString;
+}
+     
+// keep this function call here 
+console.log(FirstReverse(readline()));
+
+// https://coderbyte.com/editor/First%20Reverse:JavaScript
