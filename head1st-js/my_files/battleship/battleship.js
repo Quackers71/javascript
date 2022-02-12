594f9def688852e5b712ef089b774ee6dd952432
Quackers <robq71@gmail.com>
2022-02-12 09:27:19 +0000

    seperated fire method calls

--- head1st-js/my_files/battleship/battleship.js
+++ head1st-js/my_files/battleship/battleship.js
@@ -52,12 +52,15 @@ var model = {
 };
 
 model.fire("53");
+
 model.fire("06");
 model.fire("16");
 model.fire("26");
+
 model.fire("34");
 model.fire("24");
 model.fire("44");
+
 model.fire("12");
 model.fire("11");
 model.fire("10");
