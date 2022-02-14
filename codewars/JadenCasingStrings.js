e01130c4f3279486afb55886d9b79b575d672caa
ThePebble <robq71@gmail.com>
2022-02-14 20:50:12 +0000

    Completed Casing Strings and workings out

--- codewars/JadenCasingStrings.js
+++ codewars/JadenCasingStrings.js
@@ -1,23 +1,21 @@
 String.prototype.toJadenCase = function (str) { 
     console.log(str);
-    var newString = str.toLowerCase().split(" ");
-    console.log(newString);
-    
-    for (i = 0; i < newString.length; i++) {
-        if (newString.length[i] < newString.length) {
-            newString[i] = newString[i].charAt(0).toUpperCase()+newString[i].substring(0);
-        }
-        str = newString.join(" ");
-    }
-    console.log(newString);
-    console.log(str);
-    return newString.join(" ");
+    var fullString = str.split(" ");
+    console.log(fullString);
+    var newString = fullString.map((s) => s.charAt(0).toUpperCase()+s.substring(1)).join(" ");
+
+    return newString;
 }
-    
-// var string = "How can mirrors be real if our eyes aren't real";
+
 console.log("".toJadenCase("How can mirrors be real if our eyes aren't real"));
 
 /* "How can mirrors be real if our eyes aren't real"
    "How Can Mirrors Be Real If Our Eyes Aren't Real" */
 
 // https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/javascript
+// *** On the actual kata I had to use on line 3: var fullString = this.split(" "); ***
+
+// * Another way: return this.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join(' ') *
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+// Array.prototype.map()
--- codewars/JadenCasingStringsWorks.js
+++ codewars/JadenCasingStringsWorks.js
@@ -31,9 +31,6 @@ console.log("".toJadenCase("How can mirrors be real if our eyes aren't real"));
    "How Can Mirrors Be Real If Our Eyes Aren't Real" */
 
 // https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/javascript
-// *** On the actual kata I had to use on line 3: var fullString = this.split(" "); ***
-
-// * Another way: return this.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join(' ') *
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 // Array.prototype.map()
