9e91cf2e919bd99222b5e39cc34f7efda1b55a0a
ThePebble <robq71@gmail.com>
2022-02-18 20:27:29 +0000

    changed some stuff with JS

--- my_projects/to-do-app/index.js
+++ my_projects/to-do-app/index.js
@@ -1,3 +1,18 @@
 var newHeader = document.getElementById("todo");
+// var listItems = document.getElementById("mainList").getElementsByTagName("li");
+var listItems = document.querySelectorAll("#mainList li");
+
 newHeader.innerHTML = "To Do List";
-document.getElementById("todo").style.color = "purple";
+newHeader.style.color = "purple";
+
+listItems[0].innerHTML = "Yes 1";
+
+for (var i = 0; i < listItems.length; i++) {
+    listItems[i].innerHTML = "Ammendment "+(i+1);
+}
+
+var element = document.getElementById("button");
+var listener = element.addEventListener("click", function(e) {
+    console.log("clicked");
+});
+
