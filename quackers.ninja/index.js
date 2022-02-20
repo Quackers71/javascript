2f87273a29b5c91dd1e3ccf8064d6a5e6c9f42fc
ThePebble <robq71@gmail.com>
2022-02-20 22:17:30 +0000

    about time I added some JS to q.ninja

---
+++ quackers.ninja/index.js
@@ -0,0 +1,13 @@
+window.onload = init;
+
+function init() {
+    var image = document.getElementById("goober");
+    image.onclick = showAnswer;
+}
+
+function showAnswer() {
+    var image = document.getElementById("goober");
+    image.src = "./images/myGoober.jpg";
+}
+
+console.log("Test");
