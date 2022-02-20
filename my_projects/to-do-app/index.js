dc22d845d07d90c63644af34199c8b6c03ecd85b
ThePebble <robq71@gmail.com>
2022-02-20 10:54:18 +0000

    a few amendments

--- my_projects/to-do-app/index.js
+++ my_projects/to-do-app/index.js
@@ -5,7 +5,6 @@ var deleteItem = document.getElementById("removeItemButton");
 var newHeader = document.getElementById("mainTitle");
 // var listItems = document.getElementById("mainList").getElementsByTagName("li");
 var listItems = document.querySelectorAll("#itemList li");
-var removeLastItem = document.querySelectorAll("#itemList li");
 
 newHeader.innerHTML = "The List Items";
 newHeader.style.color = "sandybrown";
@@ -20,18 +19,10 @@ function activateItem() {
 }
 
 addItem.addEventListener("click", createNewItem);
-deleteItem.addEventListener("click", deleteLastItem);
+
 
 function createNewItem() {
     theList.innerHTML += "<li>New Item "+newItemCounter+"</li>";
     console.log(newItemCounter);
     newItemCounter++;
-}
-
-function deleteLastItem() {
-    console.log(newItemCounter);
-    // removeLastItem.parentNode.removeChild(removeLastItem);
-    console.log(removeLastItem[newItemCounter]);
-    console.log(newItemCounter);
-    --newItemCounter;
 }
