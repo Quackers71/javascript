5bf6d5d6815084dd1ff8b1cc0f57a46636d9fc2f
ThePebble <robq71@gmail.com>
2022-02-21 22:17:09 +0000

    updates to revert images - not perfect

--- quackers.ninja/index.js
+++ quackers.ninja/index.js
@@ -1,11 +1,17 @@
 window.onload = init;
 
 function init() {
-    var image = document.getElementById("goober");
-    image.onclick = showAnswer;
+  var image = document.getElementById("goober");
+  image.onclick = showAnswer;
+  image.ondblclick = revertAnswer;
 }
 
 function showAnswer() {
-    var image = document.getElementById("goober");
-    image.src = "./images/myGoober.jpg";
+  var image = document.getElementById("goober");
+  image.src = "./images/myGoober.jpg";
+}
+
+function revertAnswer() {
+  var image = document.getElementById("goober");
+  image.src = "./images/myGooberShakey.jpg";
 }
