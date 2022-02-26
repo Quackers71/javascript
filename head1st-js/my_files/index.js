abdcee769d5ebbc4c121b8c7079f42656d25bbea
ThePebble <robq71@gmail.com>
2022-02-26 09:22:06 +0000

    from Pg.443 passing functions to functions

--- head1st-js/my_files/index.js
+++ head1st-js/my_files/index.js
@@ -1,60 +1,82 @@
 window.onload = init;
 
 function init() {
-  var images = document.getElementsByTagName("img");
-  for (var i = 0; i < images.length; i++) {
-    images[i].onpointerover = showAnswer;
+  var image = document.getElementById("goober");
+  image.onpointerover = showAnswer;
+}
+
+function showAnswer() {
+  var image = document.getElementById("goober");
+  image.src = "images/goober.jpg";
+
+  setTimeout(revertAnswer, 2000);
+}
+
+function revertAnswer() {
+  var image = document.getElementById("goober");
+  image.src = "images/gooberblur.jpg";
+}
+
+// Main stuff for Pg. 443 Flying First Class
+
+var passengers = [
+  { name: "Jane Doloop", paid: true },
+  { name: "Dr. Evel", paid: true },
+  { name: "Sue Property", paid: false },
+  { name: "The Pebble", paid: true },
+  { name: "Donkey", paid: false },
+  { name: "Mapimpy", paid: false },
+  { name: "Scarls", paid: true },
+];
+
+function printPassenger(passenger) {
+  var message = passenger.name;
+  if (passenger.paid === true) {
+    message = message + " has paid";
+  } else {
+    message = message + " has not paid";
   }
+  console.log(message);
+  return false;
+}
 
-  for (var j = 0; j < images.length; j++) {
-    images[j].onpointerleave = revertAnswer;
+processPassengers(passengers, printPassenger);
+
+var allCanFly = processPassengers(passengers, checkNoFlyList);
+if (!allCanFly) {
+  console.log(
+    "The plane can't take off: we have a passenger on the no-fly-list."
+  );
+}
+
+var allPaid = processPassengers(passengers, checkNotPaid);
+if (!allPaid) {
+  console.log("The plane can't take off: not everyone has paid.");
+}
+
+function processPassengers(passengers, testFunction) {
+  for (var i = 0; i < passengers.length; i++) {
+    if (testFunction(passengers[i])) {
+      return false;
+    }
   }
-
-  var winner = function () {
-    console.log("WINNER!");
-  };
-  var loser = function () {
-    console.log("LOSER!");
-  };
-
-  var a = winner;
-  var b = loser;
-  var c = loser;
-
-  a(); // WINNER!
-  b(); // LOSER!
-
-  c = a; // winner
-  console.log(c);
-  a = b; // loser
-  console.log(a);
-  b = c; // winner
-  console.log(b);
-  c = a; // loser
-  console.log(c);
-  a = c; // loser
-  console.log(a);
-  a = b; // winner - which makes a(); at the end output "WINNER!"
-  console.log(a);
-  b = c; // loser
-  console.log(b);
-  a(); // WINNER!
+  return true;
 }
 
-function showAnswer(eventObj) {
-  var image = eventObj.target;
-
-  var name = image.id;
-  name += ".jpg";
-  image.src = "images/" + name;
-
-  setTimeout(revertAnswer, 2000, eventObj);
+function checkNoFlyList(passenger) {
+  return passenger.name === "Dr. Evel";
 }
 
-function revertAnswer(eventObj) {
-  var image = eventObj.target;
-
-  var name = image.id;
-  name += "blur.jpg";
-  image.src = "images/" + name;
+function checkNotPaid(passenger) {
+  return !passenger.paid;
 }
+
+/* function sayIt(translator) {
+  var phrase = translator("Goodbye");
+  console.log(phrase);
+}
+function hawaiianTranslator(word) {
+  if (word === "Hello") return "Aloha";
+  if (word === "Goodbye") return "Aloha";
+}
+sayIt(hawaiianTranslator); */
