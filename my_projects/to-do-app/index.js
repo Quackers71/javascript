bfce2e2e55f98b2c2ce5f5169570f7d1874706ca
ThePebble <robq71@gmail.com>
2022-02-28 21:42:22 +0000

    cleaned up todo app

--- my_projects/to-do-app/index.js
+++ my_projects/to-do-app/index.js
@@ -1,28 +1,9 @@
-var newItemCounter = 1;
 var theList = document.getElementById("itemList");
 var addItem = document.getElementById("addItemButton");
-var deleteItem = document.getElementById("removeItemButton");
-var newHeader = document.getElementById("mainTitle");
-// var listItems = document.getElementById("mainList").getElementsByTagName("li");
-var listItems = document.querySelectorAll("#itemList li");
-
-newHeader.innerHTML = "The List Items";
-newHeader.style.color = "sandybrown";
-theList.style.color = "white";
-
-for (var i = 0; i < listItems.length; i++) {
-    listItems[i].addEventListener("click", activateItem);
-}
-
-function activateItem() {
-    newHeader.innerHTML = this.innerHTML;
-}
+var addInput = document.getElementById("inputNewItem");
 
 addItem.addEventListener("click", createNewItem);
 
-
 function createNewItem() {
-    theList.innerHTML += "<li>New Item "+newItemCounter+"</li>";
-    console.log(newItemCounter);
-    newItemCounter++;
+  // code here Yo!
 }
