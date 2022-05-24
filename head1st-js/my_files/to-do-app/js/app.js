6d9123d87f84ec8f0bd777468d064d8526b8a0ac
ThePebble <robq71@gmail.com>
2022-05-24 22:00:45 +0100

    completed To Do List App

--- head1st-js/my_files/to-do-app/js/app.js
+++ head1st-js/my_files/to-do-app/js/app.js
@@ -1,61 +1,76 @@
 // Create a To Do List App With JavaScript, HTML and CSS taken from https://youtu.be/b8sUhU_eq3g  @29:53
 
+// Select the Elements
 const clear = document.querySelector(".clear");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
 const dateElement = document.getElementById("date");
 
+// classes names
 const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
 
-let LIST, id;
+// Variables
+let LIST, id, job;
+
+// Get item from local storage
 let data = localStorage.getItem("TODO");
 
+// Check if data is not empty
 if (data) {
     LIST = JSON.parse(data);
-    loadToDo(LIST);
     id = LIST.length;
+    loadList(LIST);
 }else {
     LIST = [];
     id = 0;
 }
 
-function loadToDo(array) {
+// Load items to the user's interface
+function loadList(array) {
     array.forEach(function(item) {
         addToDo(item.name, item.id, item.done, item.trash);
     });
 }
 
+// Clear the local storage
 clear.addEventListener('click', function() {
     localStorage.clear();
     location.reload();
 });
 
+// Display the Date
 let options = { weekday:'long', month:'short', day:'numeric'};
 let today = new Date();
+
 dateElement.innerHTML = today = today.toLocaleDateString("en-US", options);
 
-function addToDo(toDo, id, done) {
+// add to do Function
+function addToDo(toDo, id, done, trash) {
 
     if (trash) { return; }
+
     const DONE = done ? CHECK : UNCHECK;
     const LINE = done ? LINE_THROUGH : "";
 
-    const text = `<li class="item">
-                    <i class="co fa fa-circle-thin" job="complete" id="${id}"></i>
-                    <p class="text "> ${toDo} </p>
-                    <i class="de fa fa-trash-o" job="delete" id="${id}"></i>
-                </li>`
+    const item = `<li class="item">
+                    <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+                    <p class="text ${LINE}">${toDo}</p>
+                    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+                  </li>
+                `;
 
     const position = "beforeend";
 
-    list.insertAdjacentHTML(position, text);
+    list.insertAdjacentHTML(position, item);
 }
 
+// add an item to the list when the User presses the Enter Key
 document.addEventListener("keyup",function(event){
     if (event.keyCode == 13) {
         const toDo = input.value;
+
         if (toDo) {
             addToDo(toDo, id, false, false);
             LIST.push({
@@ -64,6 +79,7 @@ document.addEventListener("keyup",function(event){
                     done: false,
                     trash: false
                 });
+                // Add item to local storage
                 localStorage.setItem("TODO", JSON.stringify(LIST));
                 id++;
         }
@@ -71,24 +87,33 @@ document.addEventListener("keyup",function(event){
     }
 });
 
+//  Complete to do
 function completeToDo(element) {
     element.classList.toggle(CHECK);
     element.classList.toggle(UNCHECK);
     element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
     LIST[element.id].done = LIST[element.id].done ? false : true;
 }
 
+// Remove to do
 function removeToDo(element) {
     element.parentNode.parentNode.removeChild(element.parentNode);
+
     LIST[element.id].trash = true;
 }
 
+// target the items created dynamically
 list.addEventListener("click", function(event) {
-    let element = event.target; // <i class="de fa fa-trash-o" job="delete" id="0"></i>
-    const elementJOB = event.target.attributes.job.value; // delete or complete
-    if (elementJOB == "complete") {
+
+    const element = event.target; // return clicked element inside the list
+    const elementJob = element.attributes.job.value; // delete or complete
+    
+    if (elementJob == "complete") {
         completeToDo(element);
-    }else if (elementJOB == "delete") {
+    }else if (elementJob == "delete") {
         removeToDo(element);
     }
+    // Add item to local storage
+    localStorage.setItem("TODO", JSON.stringify(LIST));
 });
