edc012140f97165542a54143895f12f72cff0f4a
ThePebble <robq71@gmail.com>
2022-05-29 15:30:01 +0100

    addTodo Event Listener and CSS Styling

--- head1st-js/my_files/archive/to-do-app1/app.js
+++ head1st-js/my_files/archive/to-do-app1/app.js
@@ -1,7 +1,39 @@
 // Vanilla Javascript Project Tutorial taken from https://youtu.be/Ttf3CEsEwMQ @ 0:15:18 of 1:15:18
 
 // Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
 // Event Listeners
+todoButton.addEventListener('click', addTodo);
 
 // Functions
+function addTodo(event) {
+
+    // Prevents the form from submitting
+    event.preventDefault();
+
+    // Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    // Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // Check Mark Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("completed-btn");
+    todoDiv.appendChild(completedButton);
+    // Check Trash Button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    // Append to List
+    todoList.appendChild(todoDiv);
+    // Clear Todo Input Value
+    todoInput.value = "";
+}
