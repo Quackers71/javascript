90256fe3e154e10c2da608a38b7e028c1807a02a
ThePebble <robq71@gmail.com>
2022-05-30 13:14:33 +0100

    implemented local storage

--- head1st-js/my_files/archive/to-do-app1/app.js
+++ head1st-js/my_files/archive/to-do-app1/app.js
@@ -27,6 +27,8 @@ function addTodo(event) {
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
+    // Add Todo to localStorage
+    saveLocalTodos(todoInput.value);
     // Check Mark Button
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fas fa-check"></i>';
@@ -85,4 +87,17 @@ function filterTodo(e) {
                 break;
         }
     });
+}
+
+function saveLocalTodos(todo) {
+    // Check - Do you already have todos in local storage
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
