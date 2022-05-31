ed4d4a9de2cd923e14e108c7d87b695de451318d
ThePebble <robq71@gmail.com>
2022-05-31 17:45:25 +0100

    completed Task List App Tutorial

--- head1st-js/my_files/archive/to-do-app2/app.js
+++ head1st-js/my_files/archive/to-do-app2/app.js
@@ -1,6 +1,6 @@
 // Build a Todo List App Tutorial 
 // taken from https://youtu.be/MkESyVB4oUw 
-// currently @ 21:25 of 37:13
+// currently @ 28:12 of 37:13
 // https://github.com/TylerPottsDev/yt-js-task-list-2021
 
 // Selectors
@@ -37,7 +37,40 @@ window.addEventListener('load', () => {
 
         task_content_el.appendChild(task_input_el);
 
+        const task_actions_el = document.createElement("div");
+        task_actions_el.classList.add("actions");
+
+        const task_edit_el = document.createElement("button");
+        task_edit_el.classList.add("edit");
+        task_edit_el.innerHTML = "Edit";
+
+        const task_delete_el = document.createElement("button");
+        task_delete_el.classList.add("delete");
+        task_delete_el.innerHTML = "Delete";
+
+        task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
+
+        task_el.appendChild(task_actions_el);
+
         list_el.appendChild(task_el);
+
+        input.value = "";
+
+        task_edit_el.addEventListener('click', () => {
+            if (task_edit_el.innerText.toLowerCase() == "edit") {
+                task_input_el.removeAttribute("readonly");
+            task_input_el.focus();
+            task_edit_el.innerText = "Save";
+            } else {
+                task_input_el.setAttribute("readonly", "readonly");
+                task_edit_el.innerText = "Edit";
+            }
+        });
+
+        task_delete_el.addEventListener('click', () => {
+            list_el.removeChild(task_el);
+        });
     });
 });
 
