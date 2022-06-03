9b849499568d013cc473b8fd6da5dabf23082d7d
ThePebble <robq71@gmail.com>
2022-06-03 12:44:42 +0100

    declared Selectors for New Tasks

--- head1st-js/my_files/archive/to-do-app2/app.js
+++ head1st-js/my_files/archive/to-do-app2/app.js
@@ -8,6 +8,16 @@ const input = document.querySelector("#new-task-input");
 const addButton = document.querySelector("#new-task-submit");
 const list_el = document.querySelector("#tasks");
 
+// Selectors for New Tasks
+const task_el = document.createElement("div");
+const task_content_el = document.createElement("div");
+const task_input_el = document.createElement("input");
+const task_actions_el = document.createElement("div");
+const task_complete_el = document.createElement("button");
+const task_edit_el = document.createElement("button");
+const task_delete_el = document.createElement("button");
+
+
 // Event Listeners
 document.addEventListener('DOMContentLoaded', getTasks);
 addButton.addEventListener('click', addTask);
@@ -28,16 +38,13 @@ function addTask(e) {
     }
 
     // new task div
-    const task_el = document.createElement("div");
     task_el.classList.add("task");
 
     // new content div
-    const task_content_el = document.createElement("div");
     task_content_el.classList.add("content");
     task_el.appendChild(task_content_el);
 
     // new text input
-    const task_input_el = document.createElement("input");
     task_input_el.classList.add("text");
     task_input_el.type = "text";
     task_input_el.value = task;
@@ -47,18 +54,14 @@ function addTask(e) {
     // add task to localStorage function call
     saveLocalTasks(input.value);
 
-    const task_actions_el = document.createElement("div");
     task_actions_el.classList.add("actions");
 
-    const task_complete_el = document.createElement("button");
     task_complete_el.classList.add("complete");
     task_complete_el.innerHTML = "Complete";
 
-    const task_edit_el = document.createElement("button");
     task_edit_el.classList.add("edit");
     task_edit_el.innerHTML = "Edit";
 
-    const task_delete_el = document.createElement("button");
     task_delete_el.classList.add("delete");
     task_delete_el.innerHTML = "Delete";
 
@@ -87,12 +90,6 @@ function saveLocalTasks(task) {
 
 function editTask(task) {
 
-    const task_el = document.createElement("div");
-    const task_input_el = document.createElement("input");
-    const task_complete_el = document.createElement("button");
-    const task_edit_el = document.createElement("button");
-    const task_delete_el = document.createElement("button");
-
     if (task_complete_el.innerText.toLowerCase() == "completed") {
         alert("You are unable to amend a task, once it has been marked as completed!");
     } else {
@@ -105,7 +102,6 @@ function editTask(task) {
             task_edit_el.innerText = "Edit";
         }
     }
-
 }
 
 function deleteTask(e) {
@@ -115,10 +111,40 @@ function deleteTask(e) {
 
     if (task.classList[0] === "delete") {
         const removeTask = task.parentElement;
-        task.remove();
+        // removeLocalTasks(task);
+        task.addEventListener('transitionend', function() {
+            task.remove();
+        });
+        console.log("deleteTask")
     }
 
-    console.log("deleteTask")
+    if (task_complete_el.innerText.toLowerCase() == "completed") {
+        alert("You are unable to amend a task, once it has been marked as completed!");
+    } else {
+        if (task_edit_el.innerText.toLowerCase() == "edit") {
+            task_input_el.removeAttribute("readonly");
+            task_input_el.focus();
+            task_edit_el.innerText = "Save";
+            console.log("Edit mode");
+        } else {
+            task_input_el.setAttribute("readonly", "readonly");
+            task_edit_el.innerText = "Edit";
+        }
+    }
+    console.log("Skipped Editing")
+}
+
+function removeLocalTasks(task) {
+    // Check - Do you already have tasks in local storage
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    const taskIndex = task.children[0].innerText;
+    tasks.splice(tasks.indexOf(taskIndex), 1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 function getTasks() {
@@ -171,5 +197,5 @@ function getTasks() {
         list_el.appendChild(task_el);
 
         console.log("checked getTasks()")
-    })
+    });
 }
