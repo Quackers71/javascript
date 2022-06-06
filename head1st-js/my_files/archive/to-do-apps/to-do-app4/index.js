571c76ff14532530556b4532342b8a482923e3f1
ThePebble <robq71@gmail.com>
2022-06-06 21:41:12 +0100

    fixed it you plumhead!

--- head1st-js/my_files/archive/to-do-apps/to-do-app4/index.js
+++ head1st-js/my_files/archive/to-do-apps/to-do-app4/index.js
@@ -1,2 +1,30 @@
 //  To Do List taken from https://youtu.be/dHtyDron5ik
 
+(function(){
+    var input = document.getElementById("input");
+    var btn = document.getElementById("btn");
+    var lists = {
+        todo: document.getElementById("todo"),
+        done: document.getElementById("done")
+    };
+
+    // console.log(input, btn, lists);
+
+    /* *
+     * @param {string} str
+     * @returns {HTMLElement}
+     */
+    var makeTaskHtml = function(str) {
+        var el = document.createElement("li");
+        el.textContent = str;
+
+        return el;
+    };
+
+    var addTask = function(list, task) {
+        list.appendChild(task);
+    }
+
+    addTask(lists.todo, makeTaskHtml("Test Todo Task"));
+    addTask(lists.done, makeTaskHtml("Test Done Task"));
+}());
