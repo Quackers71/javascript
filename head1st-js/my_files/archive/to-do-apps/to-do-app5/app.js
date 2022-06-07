ef6638fd933f52de83d0ec8690cd4eb15a7db795
ThePebble <robq71@gmail.com>
2022-06-07 23:38:25 +0100

    alert on empty field

--- head1st-js/my_files/archive/to-do-apps/to-do-app5/app.js
+++ head1st-js/my_files/archive/to-do-apps/to-do-app5/app.js
@@ -1,1 +1,35 @@
 // To Do List taken from https://youtu.be/-pRg_daFjfk
+
+let addToDoButton = document.getElementById("addToDo");
+let toDoContainer = document.getElementById("toDoContainer");
+let inputField = document.getElementById("inputField");
+
+addToDoButton.addEventListener("click", function(event) {
+
+    event.preventDefault();
+
+    if (!inputField.value) {
+        alert("Please enter something!");
+        return;
+    }
+
+    var paragraph = document.createElement("p");
+    paragraph.classList.add("paragraph-styling");
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+
+    /* var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    toDoContainer.appendChild(checkbox); */
+
+    inputField.value = "";
+    inputField.focus();
+
+    paragraph.addEventListener("click", function() {
+        paragraph.style.textDecoration = "line-through";
+    });
+
+    paragraph.addEventListener("dblclick", function() {
+        toDoContainer.removeChild(paragraph);
+    });
+});
