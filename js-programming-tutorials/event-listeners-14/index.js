68d48028f29ed5b77d2d32119bb37b3956d3ef17
Quackers <robq71@gmail.com>
2022-06-08 15:12:22 +0000

    latest updates but not workin!

--- js-programming-tutorials/event-listeners-14/index.js
+++ js-programming-tutorials/event-listeners-14/index.js
@@ -1,7 +1,12 @@
 // Event Listeners - Javascript - Tutorial 14 taken from https://youtu.be/jrI0WFCCLWY
+// @ 9:17 of 13:54
 
 function handleClick(element) {
-    element.innerHTML = "Clicked";
-    element.style = "background-color: red";
+    element.style = "background-color: blue";
 }
 
+var image = document.getElementById("image");
+image.addEventListener("mouseover", function() {
+    this.style = "box-shadow: 2px 2px 2px white";
+    this.width = "300";
+});
