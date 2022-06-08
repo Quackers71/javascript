e0abbc121b53ae3e056e4a0560ec8129f8d74b4a
Quackers <robq71@gmail.com>
2022-06-08 13:47:37 +0000

    inital commit for js-prog

---
+++ js-programming-tutorials/event-listeners-14/index.js
@@ -0,0 +1,7 @@
+// Event Listeners - Javascript - Tutorial 14 taken from https://youtu.be/jrI0WFCCLWY
+
+function handleClick(element) {
+    element.innerHTML = "Clicked";
+    element.style = "background-color: red";
+}
+
