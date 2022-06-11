c59799eb3773634455b01695b12e9115e2c6fd68
ThePebble <robq71@gmail.com>
2022-06-11 17:31:54 +0100

    event listener for once the whole page has loaded

--- js-programming-tutorials/Arrays/index.js
+++ js-programming-tutorials/Arrays/index.js
@@ -2,32 +2,36 @@
 
 // object.addEventListener(event, function)
 
-let btn = document.getElementById("btn");
-let lnk = document.getElementById("lnk");
-let txt = document.getElementById("txt");
+document.addEventListener("DOMContentLoaded", init);
 
-btn.addEventListener("click", buttonClicked);
-function buttonClicked(ev) {
-    console.log(ev.type, ev.target, ev.currentTarget);
+function init() {
+    let btn = document.getElementById("btn");
+    let lnk = document.getElementById("lnk");
+    let txt = document.getElementById("txt");
+
+    btn.addEventListener("click", buttonClicked);
+    function buttonClicked(ev) {
+        console.log(ev.type, ev.target, ev.currentTarget);
+    }
+
+    // fat arrow function
+    lnk.addEventListener("click", ev => {
+        ev.preventDefault();
+        console.log(ev.type, ev.target);
+    });
+
+    txt.addEventListener("input", ev => {
+        console.log(ev.type, ev.target, ev.target.value);
+    });
+
+    txt.addEventListener("change", ev => {
+        console.log(ev.type, ev.target, ev.target.value);
+    });
+
+    txt.addEventListener("blur", ev => {
+        console.log(ev.type);
+    });
 }
-
-// fat arrow function
-lnk.addEventListener("click", ev => {
-    ev.preventDefault();
-    console.log(ev.type, ev.target);
-});
-
-txt.addEventListener("input", ev => {
-    console.log(ev.type, ev.target, ev.target.value);
-});
-
-txt.addEventListener("change", ev => {
-    console.log(ev.type, ev.target, ev.target.value);
-});
-
-txt.addEventListener("blur", ev => {
-    console.log(ev.type);
-});
 
 
 
