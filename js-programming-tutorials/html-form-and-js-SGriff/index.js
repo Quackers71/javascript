b06c4710a63137c3404a64914186def74fee6d8d
ThePebble <robq71@gmail.com>
2022-06-11 21:20:04 +0100

    form send working with validation set to true

--- js-programming-tutorials/html-form-and-js-SGriff/index.js
+++ js-programming-tutorials/html-form-and-js-SGriff/index.js
@@ -7,14 +7,34 @@ const init = function() {
     document.getElementById("button-send").addEventListener("click", send);
 }
 const reset = function(ev) {
-    
+    ev.preventDefault();
+    document.getElementById("form-user").reset();
 }
 
 const send = function(ev) {
-
+    ev.preventDefault();
+    let ret = validate();
+    if(ret) {
+        document.getElementById("form-user").submit();
+    } else {
+        // bad user
+        let err = document.querySelector(".error");
+        let input = err.querySelector("input");
+        err.setAttribute("data-errormsg", ` ... Missing ${input.placeholder}`);
+    }
 }
 
 const validate = function(ev) {
+    let valid = false;
+    let chk = document.getElementById("input-value");
+
+    let select = document.getElementById("input-age");
+
+    let first = document.getElementById("input-first");
+    let last = document.getElementById("input-last");
+    let email = document.getElementById("input-email");
+
+    return true;
 
 }
 
