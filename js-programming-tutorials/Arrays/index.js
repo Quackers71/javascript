02856f328787b6342badc4a633a1de5fd6f946d7
ThePebble <robq71@gmail.com>
2022-06-11 17:25:04 +0100

    event listeners for the 3 elements

--- js-programming-tutorials/Arrays/index.js
+++ js-programming-tutorials/Arrays/index.js
@@ -11,11 +11,40 @@ function buttonClicked(ev) {
     console.log(ev.type, ev.target, ev.currentTarget);
 }
 
-lnk.addEventListener("click", linkClicked);
+// fat arrow function
+lnk.addEventListener("click", ev => {
+    ev.preventDefault();
+    console.log(ev.type, ev.target);
+});
+
+txt.addEventListener("input", ev => {
+    console.log(ev.type, ev.target, ev.target.value);
+});
+
+txt.addEventListener("change", ev => {
+    console.log(ev.type, ev.target, ev.target.value);
+});
+
+txt.addEventListener("blur", ev => {
+    console.log(ev.type);
+});
+
+
+
+// anonymous function
+/* lnk.addEventListener("click", function(ev) {
+    ev.preventDefault();
+    console.log(ev.type, ev.target);
+}); */
+
+// named function
+/* lnk.addEventListener("click", linkClicked);
 function linkClicked(ev) {
     ev.preventDefault(); // disable hyperlink
     console.log(ev.type, ev.target, ev.currentTarget);
-}
+} */
+
+
 
 /* myDiv.addEventListener("click", doSomething);
 
