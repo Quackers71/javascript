505fd7738b8b945fc34d832f4673d17f3a57b05e
ThePebble <robq71@gmail.com>
2022-06-11 21:02:54 +0100

    initial commit for HTML Forms & JS - SGriff tute

---
+++ js-programming-tutorials/html-form-and-js-SGriff/index.js
@@ -0,0 +1,22 @@
+// HTML Forms and JavaScript - Tutorial taken from https://youtu.be/ikR9DsGMUMc
+
+
+
+const init = function() {
+    document.getElementById("button-cancel").addEventListener("click", reset);
+    document.getElementById("button-send").addEventListener("click", send);
+}
+const reset = function(ev) {
+    
+}
+
+const send = function(ev) {
+
+}
+
+const validate = function(ev) {
+
+}
+
+
+document.addEventListener("DOMContentLoaded", init);
