d70e9df8989edbaacd6ded09b43f2f2cdd7368e5
Quackers <robq71@gmail.com>
2022-09-03 18:31:14 +0100

    basic math and Ecma ISO stuff

--- js-programming-tutorials/frontend-web-dev-bc-c/index.js
+++ js-programming-tutorials/frontend-web-dev-bc-c/index.js
@@ -1,4 +1,4 @@
-// Frontend Web Development Bootcamp Course (JavaScript, HTML, CSS) via https://www.youtube.com/watch?v=zJSY8tbf_ys @ 41:00 / 21:14:41
+// Frontend Web Development Bootcamp Course (JavaScript, HTML, CSS) via https://www.youtube.com/watch?v=zJSY8tbf_ys @ 46:00 / 21:14:41
 
 const h1Body = document.querySelector('h1');
 const buttonBody = document.querySelector('button');
@@ -19,3 +19,16 @@ function onClickEvent() {
 }
 
 document.querySelector('button').onclick = onClickEvent;
+
+// basic math function
+function doMath(x, y) {
+    return x * y;
+}
+
+const result = doMath(5, 87);
+console.log(result);
+
+// ISO date and time
+const myDate = new Date();
+console.log(myDate.toISOString());
+// Output - 2022-09-03T17:22:43.080Z
